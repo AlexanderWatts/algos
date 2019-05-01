210e853f43fd347bcf6d92508b57daa15094d824
Alexander Watts <alexcharleswatts@gmail.com>
2019-05-01 11:36:55 +0100

    Created linearSearch class with searching method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import com.company.searching.BinarySearch;
+import com.company.searching.LinearSearch;
 
 public class Main {
 
@@ -10,6 +11,9 @@ public class Main {
         BinarySearch bs = new BinarySearch();
         System.out.println(bs.search(new int[]{1,2,5,9,20}, 20));
 
+        //Linear search
+        LinearSearch ls = new LinearSearch();
+        System.out.println(ls.search(new int[]{1,2,5,9,20}, 1));
 
 
     }
